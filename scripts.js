4e05c5ec373d6ae7816c34c53398f0358f324902
Michal Zet <michal.zadarnowski@gmail.com>
2018-05-17 20:50:19 +0100

    fix files

---
+++ scripts.js
@@ -0,0 +1,13 @@
+/* Get our elements */
+
+const player = document.querySelector('.player')
+const video = player = documet.querySelector('.viewer');
+const progress = player.querySekector('.progress');
+const progressBar = player.querySelector('.progress__filled');
+const toggle = player.querySelector('.toggle');
+const skipButton = player.querySelector('[data-skip]');
+const ranges = player.querySelector('.player__slider');
+
+/* Build out functions */
+
+/* Hook up the event listeners */
